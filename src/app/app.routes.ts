210effb7513ca690841b08a1cc87338c76126f05
pwijsen <paul@wijsen.be>
2025-07-01 14:56:32 +0200

    Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { FeteComponent } from './pages/fete/fete.component';
+
 import { AccueilComponent } from './pages/accueil/accueil.component';
 
 
 
 export const routes: Routes = [
   { path: '', component: AccueilComponent },
-  { path: 'fete', component: FeteComponent },
-
+ 
 ];
